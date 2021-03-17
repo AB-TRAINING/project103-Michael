89c0ada391cc95fb1d13ace4711854c4d582fad8
Michael <michaeldimakilingece@gmail.com>
2021-03-17 10:25:18 +0800

    commit

---
+++ Michael.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Welcome to Michael's Website </title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/cover/">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="assets/css/Michael.css">
+
+
+<body>
+    <nav>
+      <div class="logo">
+          <h4>Web Developer</h4>
+      </div>
+        <ul class="nav-links">
+          <li class="list-inline-item"><a href="/">Home</a>
+          </li>
+          <li class="list-inline-item"><a href="#">About</a>
+          </li>
+          <li class="list-inline-item"><a href="#">Projects</a>
+          </li>
+          <li class="list-inline-item"><a href="#">Contact</a>
+          </li>
+        </ul>
+        <div class="burger">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+    </nav>
+    <script src="/assets/js/Michael.js"></script>
+    </div>
+    <div class="container">
+        <div class="row">
+          <div class="col"><a href="/Calculator.html">Calculator</a>
+          </div>
+          <div class="col"><a href="/Calendar.html">Calendar</a>
+          </div>
+    </div>
+</body>
